6de87e8036915f0075ae20a2c56b5e4f5b91f5b8
involvedev <worpaldev@gmail.com>
2026-06-18 10:26:42 +0300

    Added basic code for launching

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import *
+
+dp = Dispatcher()
+
+async def main():
+    bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
